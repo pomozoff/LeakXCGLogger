24a099da113bb236397615ba5e216034a3a9abd2
Anton Pomozov <anton.pomozov@infotecs.ru>
2016-07-06 11:09:22 +0300

    add: Log info line

--- LeakXCGLogger/AppDelegate.swift
+++ LeakXCGLogger/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import XCGLogger
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        XCGLogger.defaultInstance().info("Test")
         return true
     }
 
